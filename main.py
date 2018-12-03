0e271d9d57b0459edd8622711aa959f5d333bb0b
Smith Jesko <smithjesko@gmail.com>
2018-12-03 15:25:17 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import pytesseract
+from translate import Translator
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import sys
